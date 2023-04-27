62ddab2846be1cfd1bb7b8425fac330ff8dd4208
lllying <57206446+lllying@users.noreply.github.com>
2023-04-27 18:56:04 +0800

    Add files via upload

---
+++ mmdet/datasets/__init__.py
@@ -0,0 +1,40 @@
+'''
+Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+Date: 2023-04-10 11:51:12
+LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+LastEditTime: 2023-04-10 12:32:57
+FilePath: /OBBDetection-LY/mmdet/datasets/__init__.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
+from .cityscapes import CityscapesDataset
+from .coco import CocoDataset
+from .custom import CustomDataset
+from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
+                               RepeatDataset)
+from .deepfashion import DeepFashionDataset
+from .lvis import LVISDataset
+from .samplers import DistributedGroupSampler, DistributedSampler, GroupSampler
+from .voc import VOCDataset
+from .wider_face import WIDERFaceDataset
+from .xml_style import XMLDataset
+
+from .obb.dota import DOTADataset
+from .obb.dior import DIORDataset
+from .obb.hrsc import HRSCDataset
+from .obb.msra_td500 import MSRATD500Dataset
+from .obb.rctw17 import RCTW17Dataset
+from .obb.isaid import ISAIDDataset
+from .SAR.SSDD import SSDDDataset
+from .SAR.SSDDPLUS import SSDDPLUSDataset
+
+__all__ = [
+    'CustomDataset', 'XMLDataset', 'CocoDataset', 'DeepFashionDataset',
+    'VOCDataset', 'CityscapesDataset', 'LVISDataset', 'GroupSampler',
+    'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset',
+    'CityscapesDataset', 'LVISDataset', 'DeepFashionDataset', 'GroupSampler',
+    'DistributedGroupSampler', 'DistributedSampler', 'build_dataloader',
+    'ConcatDataset', 'RepeatDataset', 'ClassBalancedDataset',
+    'WIDERFaceDataset', 'DATASETS', 'PIPELINES', 'build_dataset',
+    'SSDDDataset','SSDDPLUSDataset'
+]
